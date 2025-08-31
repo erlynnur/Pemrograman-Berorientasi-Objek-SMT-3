af882f8a439ff5de94c1b7aa14b730f28c36ad52
erlynnur <erlynmaulidya28@gmail.com>
2025-08-31 08:29:45 +0700

    Add files via upload

---
+++ ErlynPBO1/src/main/java/com/mycompany/erlynpbo1/ErlynPBO1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.erlynpbo1;
+
+/**
+ *
+ * @author LOQ
+ */
+public class ErlynPBO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Erlyn Nur Rizqi Maulidya");
+        System.out.println("NPM     : 2430506067");
+        System.out.println("Alamat  : Dumpoh, Magelang");
+        System.out.println("No_HP   : 088888888854");
+    }
+}
